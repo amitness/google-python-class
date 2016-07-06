5aee8d29928295ca74d0fac2ab53d136499f6504
Amit Chaudhary <studenton.com@gmail.com>
2016-07-06 21:47:17 +0545

    Solve day 1 part 2 exercises

--- basic/list1.py
+++ basic/list1.py
@@ -21,8 +21,7 @@
 # and last chars of the string are the same.
 # Note: python does not have a ++ operator, but += works.
 def match_ends(words):
-  # +++your code here+++
-  return
+  return len([word for word in words if len(word)>=2 and word[0] == word[-1]])
 
 
 # B. front_x
@@ -33,10 +32,7 @@ def match_ends(words):
 # Hint: this can be done by making 2 lists and sorting each of them
 # before combining them.
 def front_x(words):
-  # +++your code here+++
-  return
-
-
+  return sorted(words, key = lambda word: (not word.startswith("x"),word))
 
 # C. sort_last
 # Given a list of non-empty tuples, return a list sorted in increasing
@@ -45,8 +41,7 @@ def front_x(words):
 # [(2, 2), (1, 3), (3, 4, 5), (1, 7)]
 # Hint: use a custom key= function to extract the last element form each tuple.
 def sort_last(tuples):
-  # +++your code here+++
-  return
+  return sorted(tuples, key = lambda tuple: tuple[-1])
 
 
 # Simple provided test() function used in main() to print
@@ -75,7 +70,7 @@ def main():
   test(front_x(['mix', 'xyz', 'apple', 'xanadu', 'aardvark']),
        ['xanadu', 'xyz', 'aardvark', 'apple', 'mix'])
 
-       
+
   print
   print 'sort_last'
   test(sort_last([(1, 3), (3, 2), (2, 1)]),
